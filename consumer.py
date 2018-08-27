cc4b0c98bf211dd579755de05a98914baf295d39
SivaMaplelabs <sivasankar.boomarapu@maplelabs.com>
2018-08-27 09:07:24 +0530

    Address the pylint warnings

--- consumer.py
+++ consumer.py
@@ -1,20 +1,19 @@
-from kafka import KafkaConsumer
+import io
 import avro.schema
 import avro.io
-import io
+from kafka import KafkaConsumer
 
 # To consume messages
-consumer = KafkaConsumer('my-topic',
+CONSUMER = KafkaConsumer('my-topic',
                          group_id='my_group',
                          bootstrap_servers=['localhost:9092'])
 
-schema_path="user.avsc"
-schema = avro.schema.parse(open(schema_path).read())
+SCHEMA_PATH = "user.avsc"
+SCHEMA = avro.schema.parse(open(SCHEMA_PATH).read())
 
-for msg in consumer:
+for msg in CONSUMER:
     bytes_reader = io.BytesIO(msg.value)
     decoder = avro.io.BinaryDecoder(bytes_reader)
-    reader = avro.io.DatumReader(schema)
+    reader = avro.io.DatumReader(SCHEMA)
     user1 = reader.read(decoder)
     print user1
-	
--- producer.py
+++ producer.py
@@ -1,24 +1,25 @@
-from kafka import SimpleProducer, KafkaClient
+import io
+import random
 import avro.schema
-import io, random
 from avro.io import DatumWriter
+from kafka import SimpleProducer
+from kafka import KafkaClient
 
 # To send messages synchronously
-kafka = KafkaClient('localhost:9092')
-producer = SimpleProducer(kafka)
- 
+KAFKA = KafkaClient('localhost:9092')
+PRODUCER = SimpleProducer(KAFKA)
+
 # Kafka topic
-topic = "my-topic"
+TOPIC = "my-topic"
 
 # Path to user.avsc avro schema
-schema_path="user.avsc"
-schema = avro.schema.parse(open(schema_path).read())
- 
- 
+SCHEMA_PATH = "user.avsc"
+SCHEMA = avro.schema.parse(open(SCHEMA_PATH).read())
+
 for i in xrange(10):
-    writer = avro.io.DatumWriter(schema)
+    writer = DatumWriter(SCHEMA)
     bytes_writer = io.BytesIO()
     encoder = avro.io.BinaryEncoder(bytes_writer)
-    writer.write({"name": "123", "favorite_color": "111", "favorite_number": random.randint(0,10)}, encoder)
+    writer.write({"name": "123", "favorite_color": "111", "favorite_number": random.randint(0, 10)}, encoder)
     raw_bytes = bytes_writer.getvalue()
-    producer.send_messages(topic, raw_bytes)
+    PRODUCER.send_messages(TOPIC, raw_bytes)
